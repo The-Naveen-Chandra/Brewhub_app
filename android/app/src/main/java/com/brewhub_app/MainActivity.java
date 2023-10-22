93488eaa88c9279cab3ae738f6cef1edaf149ec5
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-10-23 01:34:20 +0530

    Assets are added

--- android/app/src/main/java/com/brewhub_app/MainActivity.java
+++ android/app/src/main/java/com/brewhub_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.brewhub_app;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Brewhub_app";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
