438d244828de535d4a88fda30dd4168c39ed04ca
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-10-27 18:49:30 +0530

    Dev. Upper Part of  Detail Screen & its component

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,13 +1,59 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {useStore} from '../store/store';
+import {COLORS} from '../theme/theme';
+import ImageBackgroundInfo from '../components/ImageBackgroundInfo';
 
-const DetailsScreen = () => {
+const DetailsScreen = ({navigation, route}: any) => {
+  const ItemOfIndex = useStore((state: any) =>
+    route.params.type == 'Coffee' ? state.CoffeeList : state.BeanList,
+  )[route.params.index];
+
+  const addToFavoriteList = useStore((state: any) => state.addToFavoriteList);
+  const deleteFromFavoriteList = useStore(
+    (state: any) => state.deleteFromFavoriteList,
+  );
+
+  const ToggleFavorite = (favorite: boolean, type: string, id: string) => {
+    favorite ? deleteFromFavoriteList(type, id) : addToFavoriteList(type, id);
+  };
+
+  const BackHandler = () => {
+    navigation.pop();
+  };
+
   return (
-    <View>
-      <Text>DetailsScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <ImageBackgroundInfo
+          EnableBackHandler={true}
+          imagelink_portrait={ItemOfIndex.imagelink_portrait}
+          type={ItemOfIndex.type}
+          id={ItemOfIndex.id}
+          favorite={ItemOfIndex.favorite}
+          name={ItemOfIndex.name}
+          special_ingredient={ItemOfIndex.special_ingredient}
+          ingredients={ItemOfIndex.ingredients}
+          average_rating={ItemOfIndex.average_rating}
+          ratings_count={ItemOfIndex.ratings_count}
+          roasted={ItemOfIndex.roasted}
+          BackHandler={BackHandler}
+          ToggleFavorite={ToggleFavorite}
+        />
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {flexGrow: 1},
+});
+
 export default DetailsScreen;
